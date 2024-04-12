3a55be182a3e87941b3d6ee13daabac539f920e6
Alissandra <alissandra.o.t@gmail.com>
2024-04-12 18:11:22 -0300

    adicionado teste de inserção de 2 comentários

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -2,9 +2,41 @@ import { fireEvent, render, screen } from '@testing-library/react';
 import Post from '.';
 import PostComment from '.';
 
-describe('Teste para o componente PostComment', () => {
+const comments = [
+    {
+        comment: 'teste 1', 
+        id: 1
+    },
+    {
+        comment: 'teste 2', 
+        id: 2
+    }
+]
+
+describe('Teste para o componente PostComment', () => { 
     it('Deve renderizar o componente corretamente', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
-    });
+    })
+
+    test('Deve renderizar com dois comentários', () =>{
+        const {debug} = render(<PostComment  />)
+
+        fireEvent.change(screen.getByTestId('campo-comment'), {
+            target: {
+                value: 'Comentario Teste 1'
+            }    
+        })
+        fireEvent.click(screen.getByTestId('btn-comment'))
+
+        fireEvent.change(screen.getByTestId('campo-comment'), {
+            target: {
+                value: 'Comentario Teste 2'
+            }    
+        })
+        fireEvent.click(screen.getByTestId('btn-comment'))
+        debug()
+        expect(screen.getAllByTestId('list-comment')).toHaveLength(2)
+
+    })
 });
